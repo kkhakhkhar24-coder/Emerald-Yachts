0e85af4f6e165c0383170858bc2c98d94c99c98c
krupalikhakhkhar <khakhkharkrupali@gmail.com>
2026-03-31 13:12:12 +0530

    ScenicAntarctica

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import EmeraldYachts from '../src/pages/EmeraldYachts/EmeraldYachts'
 import DrakePassagePage from '../src/pages/DrakePassagePage/DrakePassagePage'
 import ScenicVSSilversea from '../src/pages/ScenicVSSilversea/ScenicVSSilversea'
+import ScenicAntarctica from '../src/pages/ScenicAntarctica/ScenicAntarctica'
 import { BrowserRouter } from 'react-router-dom'
 import { Routes } from 'react-router-dom'
 import { Route } from 'react-router-dom'
@@ -15,6 +16,7 @@ function App() {
           <Route path='/' element={<EmeraldYachts />}></Route>
           <Route path='/DrakePassagePage' element={<DrakePassagePage />}></Route>
           <Route path='/ScenicVSSilversea' element={<ScenicVSSilversea />}></Route>
+          <Route path='/ScenicAntarctica' element={<ScenicAntarctica />}></Route>
         </Routes>
       </BrowserRouter>
     </>
