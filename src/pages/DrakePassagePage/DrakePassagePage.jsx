5b71fe8b58f9200debdbb95ff99e1469c317ad25
MadhavR97 <rathodmadhav215@gmail.com>
2026-04-03 00:25:14 +0530

    Fixed Image Height

--- src/pages/DrakePassagePage/DrakePassagePage.jsx
+++ src/pages/DrakePassagePage/DrakePassagePage.jsx
@@ -290,7 +290,7 @@ export default function DrakePassagePage() {
                       style={{
                         objectFit: "cover",
                         width: "100%",
-                        height: "100%",
+                        height: "300px",
                       }}
                     />
                   </div>
