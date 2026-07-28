bfe4b9f730d600096837b0ff348b1f454ec3dcd4
MadhavR97 <rathodmadhav215@gmail.com>
2026-07-28 18:50:18 +0530

    Add ScenicSubmarineExperience files to resolve build error

---
+++ src/pages/ScenicSubmarineExperience/ScenicSubmarineExperience.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ScenicSubmarineExperience.css';
+
+const ScenicSubmarineExperience = () => {
+  return (
+    <div className="scenic-submarine-experience">
+      <h1>Scenic Submarine Experience</h1>
+    </div>
+  );
+};
+
+export default ScenicSubmarineExperience;
